3ec27fad33b25681766d834d87f1fee49e659128
coalman321 <cjtucker321@gmail.com>
2021-06-02 22:02:14 -0400

    We did it reddit!

--- src/interface.cpp
+++ src/interface.cpp
@@ -8,6 +8,7 @@
 #include <memory>
 #include <sstream>
 #include <string>
+#include <tuple>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/bool.hpp"
@@ -28,16 +29,94 @@ struct GpioPort {
     std::string chip = "gpiochip0";
     short pinDir = 0;   //0 is output, 1 is input, 2 is safety and any other is as is
     short pullDir = 0;  // 0 is no change, 1 is up 2 is down
-
     std::string topic;
+
+    // comparison operators needed for mapping
+    friend bool operator<(const GpioPort & l, const GpioPort & r){
+        return std::tie(l.chip, l.port) < std::tie(r.chip, r.port);
+    }
+    friend bool operator> (const GpioPort& lhs, const GpioPort& rhs){ return rhs < lhs; }
+    friend bool operator<=(const GpioPort& lhs, const GpioPort& rhs){ return !(lhs > rhs); }
+    friend bool operator>=(const GpioPort& lhs, const GpioPort& rhs){ return !(lhs < rhs); }
 };
 
 std::shared_ptr<std::vector<GpioPort>> createGpioMap(TiXmlDocument* doc);
 
+class LineCaller {
+private:
+    std::shared_ptr<gpiod::chip> gpioChip;
+    std::shared_ptr<gpiod::line> line;
+    std::shared_ptr<short> pinDir;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr pub;
+public:
+    LineCaller(std::string & chip, int port, short pullDir, short pinDirConf){
+        gpioChip = std::make_shared<gpiod::chip>(chip, 1);
+        line = std::make_shared<gpiod::line>(gpioChip->get_line(port));
+        pinDir = std::make_shared<short>(pinDirConf);
+
+        int dir = 0;
+        switch (*pinDir) {
+        case 0:
+            dir = gpiod::line_request::DIRECTION_INPUT;
+            break;
+        case 1:
+            dir = gpiod::line_request::DIRECTION_OUTPUT;
+            break;
+        case 2:
+            dir = gpiod::line_request::DIRECTION_OUTPUT;
+            break;
+        default:
+            dir = gpiod::line_request::DIRECTION_AS_IS;
+        }
+
+        std::bitset<32UL> flags = 0;
+        #ifdef ALLOW_PULL_DIR
+        switch (pullDir) {
+        case 1:
+            flags |= gpiod::line_request::FLAG_BIAS_PULL_UP;
+            break;
+        case 2:
+            flags |= gpiod::line_request::FLAG_BIAS_PULL_DOWN;
+            break;
+        default:
+            flags |= gpiod::line_request::FLAG_BIAS_DISABLE;
+            break;
+        }
+        #endif
+
+        line->request({"hw_interface", dir, flags});
+    }
+
+    void setPublisher(rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr publisher){
+        pub = publisher;
+    }
+
+    short getDir(){
+        return *(pinDir);
+    }
+
+    void setVal(bool val){
+        if(*pinDir > 0) line->set_value(val);
+    }
+
+    void updateCallback(std::shared_ptr<std_msgs::msg::Bool> msg){
+        setVal(msg->data);
+    }
+
+    void readCallback(){
+        if(*pinDir == 0){
+            std_msgs::msg::Bool msg = std_msgs::msg::Bool();
+            msg.data = line->get_value();
+            pub->publish(msg);
+        }
+    }
+
+};
+
 class HardwareController : public rclcpp::Node {
 private:
     //map of all gpio lines requested by system
-    std::map<std::shared_ptr<GpioPort>, std::shared_ptr<gpiod::line>> lines;
+    std::map<GpioPort, std::shared_ptr<LineCaller>> lines;
 
     //list of all subscribed topics
     std::vector<std::string> topics;
@@ -66,43 +145,16 @@ public:
         safetyTimer = create_wall_timer(SAFETY_TIMEOUT_CHECK, std::bind(&HardwareController::safetyTimerUpdate, this));
     }
 
-    void registerGpio(std::vector<std::shared_ptr<GpioPort>> ports) {
+    void registerGpio(std::vector<GpioPort> ports) {
         try {
             //create all GPIO
             for (auto it = ports.begin(); it != ports.end(); it++) {
-                std::shared_ptr<gpiod::chip> gpioChip = std::make_shared<gpiod::chip>((*it)->chip, 1);
-                std::shared_ptr<gpiod::line> line = std::make_shared<gpiod::line>(gpioChip->get_line((*it)->port));
-                int dir = 0;
-                switch (it->get()->pinDir) {
-                case 0:
-                    dir = gpiod::line_request::DIRECTION_INPUT;
-                    break;
-                case 1:
-                    dir = gpiod::line_request::DIRECTION_OUTPUT;
-                    break;
-                case 2:
-                    dir = gpiod::line_request::DIRECTION_OUTPUT;
-                    break;
-                default:
-                    dir = gpiod::line_request::DIRECTION_AS_IS;
-                }
+                //RCLCPP_INFO(this->get_logger(), "Creating LineCaller %s %d %d %d", it->chip.c_str(), it->port, it->pullDir, it->pinDir);
 
-                std::bitset<32UL> flags = 0;
-                #ifdef ALLOW_PULL_DIR
-                switch (it->get()->pullDir) {
-                case 1:
-                    flags |= gpiod::line_request::FLAG_BIAS_PULL_UP;
-                    break;
-                case 2:
-                    flags |= gpiod::line_request::FLAG_BIAS_PULL_DOWN;
-                    break;
-                default:
-                    flags |= gpiod::line_request::FLAG_BIAS_DISABLE;
-                    break;
-                }
-                #endif
+                std::shared_ptr<LineCaller> line = std::make_shared<LineCaller>(it->chip, it->port, it->pullDir, it->pinDir);
 
-                line->request({"hw_interface", dir, flags});
+                lineSubscriptions.push_back(this->create_subscription<std_msgs::msg::Bool>(it->topic, 10,
+                                                                                                   std::bind(&LineCaller::updateCallback, line, _1)));
 
                 lines[*it] = line;
             }
@@ -121,18 +173,23 @@ public:
     }
 
     void safetyTimerUpdate() {
+        //RCLCPP_INFO(this->get_logger(), "Safety timer tick");
         auto now = std::chrono::system_clock::now().time_since_epoch();
-        if (safetyEnable && (now - SAFETY_TIMEOUT > safetyStamp))
+        if (safetyEnable && (now - SAFETY_TIMEOUT > safetyStamp)){
+            RCLCPP_INFO(this->get_logger(), "Safety timer expired");
             safetyEnable = false;
+            safe();
+        }
+            
         if (safetyEnable && (now - (SAFETY_TIMEOUT / 2) > safetyStamp))
             safetyToggle = !safetyToggle;
     }
 
     void safe() {
-        std::map<std::shared_ptr<GpioPort>, std::shared_ptr<gpiod::line>>::iterator it;
+        std::map<GpioPort, std::shared_ptr<LineCaller>>::iterator it;
         for (it = lines.begin(); it != lines.end(); it++) {
-            if (it->second->direction() == (int)gpiod::line::DIRECTION_OUTPUT) {
-                it->second->set_value(0);
+            if (it->second->getDir() > 0) {
+                it->second->setVal(0);
             }
         }
     }
@@ -142,14 +199,13 @@ int main(int argc, char** argv) {
     //init ros node
     rclcpp::init(argc, argv);
     std::shared_ptr<HardwareController> rosNode = std::make_shared<HardwareController>();
-    RCLCPP_INFO(rosNode->get_logger(), "hardware interface node starting");
+    RCLCPP_INFO(rosNode->get_logger(), "Hardware interface node starting");
 
     // Load the xml file
     std::filesystem::path config = std::filesystem::current_path() / "config.xml";
     TiXmlDocument* doc = new TiXmlDocument(config.c_str());
     if (!doc->LoadFile()) {
         RCLCPP_ERROR(rosNode->get_logger(), "Error parsing XML config %s\n %s", config.c_str(), doc->ErrorDesc());
-
     } else {
         try {
             // grab the parent motor XML element and make sure it exists
@@ -158,12 +214,13 @@ int main(int argc, char** argv) {
             TiXmlElement* gpios = hardware->FirstChildElement("gpios");
             if (!gpios) throw std::runtime_error("XML doc is missing gpios element. The gpios element should be defined even if there are no gpio being created");
 
-            std::vector<std::shared_ptr<GpioPort>> gpioList = std::vector<std::shared_ptr<GpioPort>>();
+           // RCLCPP_INFO(rosNode->get_logger(), "XML doc loaded, outer parts intact");
+            std::vector<GpioPort> gpioList = std::vector<GpioPort>();
 
             for (TiXmlElement* gpio = gpios->FirstChildElement("gpio"); gpio != nullptr; gpio = gpio->NextSiblingElement("gpio")) {
-                std::shared_ptr<GpioPort> port = std::make_shared<GpioPort>();
+                GpioPort port = {};
                 std::string tmp, tmp1;
-                if (!getValue(gpio, "topic", port->topic)) {
+                if (!getValue(gpio, "topic", port.topic)) {
                     throw std::runtime_error("Gpio definition missing topic name");
                 }
                 if (!getValue(gpio, "port", tmp)) {
@@ -172,27 +229,33 @@ int main(int argc, char** argv) {
                 if (!getValue(gpio, "dir", tmp1)) {
                     throw std::runtime_error("Gpio definition missing dir");
                 }
-                port->port = std::stoi(tmp);
-                port->pinDir = std::stoi(tmp1);
 
-                if(getValue(gpio, "pull", tmp)) port->pullDir = std::stoi(tmp);
+                RCLCPP_INFO(rosNode->get_logger(), "Got line config Topic: %s  Port: %s  Dir: %s", port.topic.c_str(), tmp.c_str(), tmp1.c_str());
+
+                port.port = std::stoi(tmp);
+                if(tmp1 == "OUT") port.pinDir = 1;
+                else if (tmp1 == "IN") port.pinDir = 0;
+                else port.pinDir = 3;
+
+                if(getValue(gpio, "pull", tmp)) port.pullDir = std::stoi(tmp);
 
                 gpioList.push_back(port);
-                
             }
 
             RCLCPP_INFO(rosNode->get_logger(), "Recieved config for %d gpio(s)", gpioList.size());
             rosNode->registerGpio(gpioList);
 
+            //RCLCPP_INFO(rosNode->get_logger(), "Registered GPIO Lines");
+
             //set all gpio to off
             rosNode->safe();
 
-            RCLCPP_INFO(rosNode->get_logger(), "hardware interface node loaded using gpiod interface");
+            RCLCPP_INFO(rosNode->get_logger(), "Hardware interface node loaded using gpiod interface");
 
             // serve the callbacks
             rclcpp::spin(rosNode);
 
-            RCLCPP_INFO(rosNode->get_logger(), "hardware interface shutting down");
+            RCLCPP_INFO(rosNode->get_logger(), "Hardware interface shutting down");
 
             //set all gpio to off
             rosNode->safe();
@@ -205,7 +268,7 @@ int main(int argc, char** argv) {
 
     delete doc;
 
-    RCLCPP_INFO(rosNode->get_logger(), "hardware interface shut down complete");
+    RCLCPP_INFO(rosNode->get_logger(), "Hardware interface shut down complete");
 
     rclcpp::shutdown();
 
